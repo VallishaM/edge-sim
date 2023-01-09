4ba92e1fa2cc37ab1a7fdb0e4ecc40036a7a95b3
VallishaM <71862949+VallishaM@users.noreply.github.com>
2023-01-09 18:33:13 +0530

    Update edgeDevice.py

--- edgeDevice.py
+++ edgeDevice.py
@@ -14,24 +14,24 @@ class Task:
 
 
 class EdgeDevice:
-    def __init__(self, uplink_speed, process_rate, bus_speed):
+    def __init__(self, uplink_speed, clock_rate, bus_speed):
         self.uplink_speed = uplink_speed  # Uplink bandwidth speed
-        self.process_rate = process_rate  # No. of bits that can be processed in 1ms
+        self.clock_rate = clock_rate  # No. of bits that can be processed in 1ms
         self.bus_speed = bus_speed  # Transfer speed from secondary storage to ram in megabits per sec
         self.upload_queue = []
         self.process_queue = []
         self.num_of_tasks = 0
 
     def generate_task(self, start_time):
-        prob = random.choice(a=[0, 1], p=[0.2, 0.8])
+        prob = random.choice(a=[0, 1], p=[0.7, 0.3])
         if prob == 1:
             task_size = (
-                random.randint(32 * 8, 4 * 1024 * 8) * 10 ** 6
-            )  # 32 megabits to 4 gigabits
+                random.randint(30, 80) * 10**6 / 10
+            )  # 20 megabits to 50 megabits
             task_timeout = round(
-                random.normal(loc=5000, scale=500)
+                random.normal(loc=10, scale=2) * 2
             )  # mean = 500ms, std deviation = 300ms => 1 time step = 500ms
-            cycles_per_bit = random.randint(1, 4)
+            cycles_per_bit = random.randint(28, 32) / 100
             return Task(task_size, task_timeout, cycles_per_bit, start_time)
         else:
             return None
@@ -41,49 +41,77 @@ class EdgeDevice:
         return True if prob >= 0.5 else False
 
     def execution_time(self, task: Task) -> int:
-        latency = (task.task_size * 1 / self.bus_speed) + (
-            task.task_size * 1 / self.process_rate
-        ) * task.cycles_per_bit
-        latency_timestep = int(math.ceil(latency / 500))
+        latency = (task.task_size * 1 / self.bus_speed) * 1000 + (
+            task.task_size * task.cycles_per_bit * 1000
+        ) / self.clock_rate
+        latency_timestep = int(math.ceil(latency / 100))
         return latency_timestep
 
     def upload_time(self, task: Task) -> int:
         up_time = task.task_size * 1 / self.uplink_speed
-        upload_timestep = int(math.ceil(up_time / 500))
+        upload_timestep = int(math.ceil(up_time / 100))
+
         return upload_timestep
 
-    def run(self, timestep):
+    def poll(self, timestep):
         task = self.generate_task(timestep)
         if task is not None:
             self.num_of_tasks += 1
             if self.policy(task):
                 # Offload
                 task.upload_latency = self.upload_time(task) + self.compute_delay("U")
-                self.upload_queue.append((task, timestep))
-                return 0
+                self.upload_queue.append((task, timestep, self.upload_time(task)))
+                return (
+                    True,
+                    True,
+                    task,
+                    False,
+                )  # (Generated?,Offload?,Latency,Dropout?)
             else:
                 # Do not offload
                 compute_latency = self.execution_time(task) + self.compute_delay("P")
                 # Drop the task if task will not get executed within timeout
                 if compute_latency + timestep <= task.start_time + task.task_timeout:
-                    self.process_queue.append((task, timestep, compute_latency,))
-                    return 0
+                    self.process_queue.append(
+                        (
+                            task,
+                            timestep,
+                            self.execution_time(task),
+                        )
+                    )
+                    return (
+                        True,
+                        False,
+                        compute_latency,
+                        False,
+                        self.execution_time(task),
+                    )  # (generated?,offloaded?,latency,dropped?,process time)
                 else:
                     # Drop task
-                    return -1
+                    return (
+                        True,
+                        False,
+                        compute_latency,
+                        True,
+                        0,
+                    )
         else:
-            return 0
+            return (False, False, -2, False)
 
     def compute_delay(self, which_queue):
         queue = []
         if which_queue == "P":
             queue = self.process_queue
-            return queue[-1][2] if len(queue) > 0 else 0
+
         elif which_queue == "U":
             queue = self.upload_queue
-            return queue[-1][0].upload_latency if len(queue) > 0 else 0
+
         else:
             return None
+        delay = 0
+        for task_tuple in queue:
+            delay += task_tuple[2]
+        return delay
 
     def refresh_upload_queue(self, timestep):
         popped = []
