576e6bf0d5c0310bbd4089d8683f0ceefa665651
VallishaM <71862949+VallishaM@users.noreply.github.com>
2023-01-09 18:33:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # ALGORITHM FOR main.py
 from server import MECServer
 from edgeDevice import EdgeDevice
-from tabulate import tabulate
+from helper import plot, plot_single
+
+# from tabulate import tabulate
 
 NUMBER_OF_MOBILE_DEVICES = 1
 offload_dictionary = {}
@@ -10,54 +12,136 @@ global_result = []
 # initialise server and devices
 devices = []
 for _ in range(0, NUMBER_OF_MOBILE_DEVICES):
-    devices.append(EdgeDevice(600 * (10 ** 3), 128 * (10 ** 6), 4200 * 8 * (10 ** 3)))
+    devices.append(EdgeDevice(14 * 10**3, 2.5 * 10**6, 4200 * 8 * (10**3)))
 server = MECServer()
 # initialise server and devices
-
+tasks_generated = 0
 time_step = 0
 tasks_dropped = 0
-while time_step < 100000:
-    global_result.extend(
-        server.refresh_process_queue(time_step)
+global_latency = []
+time_array = []
+global_upload_latency = []
+global_process_latency = []
+global_local_latency = []
+while time_step < 10000:
+    time_array.append(time_step)
+    new_tasks = 0
+    drop = 0
+    new_latency = 0
+
+    server.refresh_process_queue(
+        time_step
     )  # remove tasks from server's process queue that have been processed
     for device in devices:
-        popped = device.refresh_upload_queue(
+        device.refresh_upload_queue(
             time_step
         )  # remove tasks from device's upload queue that have been uploaded
-        for task in popped:
-            # This way we won't need an additional data structure in main.py to store the task until it's uploaded
-            tasks_dropped += server.offload(task, time_step)
-        global_result.extend(device.refresh_process_queue(time_step))
-        if time_step < 1000:
-            tasks_dropped += device.run(time_step)
+
+        device.refresh_process_queue(time_step)
+        if time_step < 10000:
+
+            result = device.poll(time_step)
+            if result[0]:  # If task generated
+                new_tasks += 1
+                if result[1]:  # If Offload
+                    # print(result)
+                    global_upload_latency.append(result[2].upload_latency)
+                    server_result = server.offload(
+                        result[2], result[2].upload_latency + time_step
+                    )
+                    if len(global_local_latency) == 0:
+                        global_local_latency.append(0)
+                    else:
+                        global_local_latency.append(global_local_latency[-1])
+
+                    global_process_latency.append(server_result[1])
+                    new_latency += server_result[1] + result[2].upload_latency
+                    global_latency.append(server_result[1] + result[2].upload_latency)
+                    if server_result[0]:  #  drop
+                        drop += 1
+
+                        print(
+                            "Offload and drop, energy:",
+                            (result[2].upload_latency + server_result[1])
+                            * 0.05
+                            * 0.001
+                            * 6.87,
+                            "Upload latency",
+                            result[2].upload_latency,
+                        )
+                    else:  # Successfully processable
+
+                        print(
+                            "Offload",
+                            server_result[1] + result[2].upload_latency,
+                            "energy:",
+                            (server_result[1] + result[2].upload_latency) * 0.05 * 6.87,
+                            "Upload latency",
+                            result[2].upload_latency,
+                        )
+                    # plot(global_latency, global_upload_latency, global_process_latency)
+                else:  # if not offload
+                    new_latency += result[2]
+                    global_latency.append(result[2])
+                    global_local_latency.append(result[4])
+                    if result[3]:  # Droop
+                        drop += 1
+                        print(
+                            "Local and drop, energy:", result[2] * 350 * 0.05
+                        )  # in milli Joule
+                    else:
+                        print(
+                            "Local : ", result[2], "energy : ", result[2] * 0.05 * 350
+                        )
+
+                    if len(global_latency) > 0:
+                        if len(global_process_latency) > 0:
+                            global_upload_latency.append(global_upload_latency[-1])
+                            global_process_latency.append(global_process_latency[-1])
+                        else:
+                            global_upload_latency.append(0)
+                            global_process_latency.append(0)
+                    plot(
+                        global_latency,
+                        global_upload_latency,
+                        global_process_latency,
+                        global_local_latency,
+                    )
+                print(
+                    "New Latency : ",
+                    new_latency,
+                    "New Tasks : ",
+                    new_tasks,
+                    "New Drop : ",
+                    drop,
+                    "Rate : ",
+                    round(drop / new_tasks, 4),
+                )
+            else:
+                print("No task generated")
+                if len(global_latency) > 0:
+                    global_latency.append(global_latency[-1])
+                    global_upload_latency.append(global_upload_latency[-1])
+                    global_process_latency.append(global_process_latency[-1])
+                    global_local_latency.append(global_local_latency[-1])
+                else:
+                    global_local_latency.append(0)
+                    global_upload_latency.append(0)
+                    global_process_latency.append(0)
+
+    tasks_dropped += drop
+    tasks_generated += new_tasks
+
+    if tasks_generated > 0:
+        print(
+            "Total Tasks : ",
+            tasks_generated,
+            "Total Drop : ",
+            tasks_dropped,
+            "Rate : ",
+            round(tasks_dropped / tasks_generated, 4),
+        )
+
     time_step += 1
 
 # Calculate drop rate
-num_of_tasks = 0
-for (idx, device) in enumerate(devices):
-    num_of_tasks += device.num_of_tasks
-    if len(device.upload_queue) > 0:
-        print(
-            f"Number of tasks left in upload queue of device {idx}: {len(device.upload_queue)}"
-        )
-    if len(device.process_queue) > 0:
-        print(
-            f"Number of tasks left in process queue of device {idx}: {len(device.process_queue)}"
-        )
-print("Number of tasks left in process queue of server: ", len(server.process_queue))
-print("Number of tasks generated: ", num_of_tasks)
-print("Task drop rate: ", (-1 * tasks_dropped / num_of_tasks))
-
-# Display the compiled results
-table = []
-for res in global_result:
-    if len(res) > 0:
-        table.append(
-            [
-                res[0].task_size,
-                res[0].task_timeout,
-                res[0].upload_latency + res[1],
-                res[0].start_time,
-            ]
-        )
-print(tabulate(table, headers=["Task Size", "Task Timeout", "Latency", "Start Time"]))
